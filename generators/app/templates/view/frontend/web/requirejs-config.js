d41ada7ccb7800d0d3e31ae476f5cb8d85ced481
Alexandru (proatwork) Bangala <proatwork1@gmail.com>
2019-06-01 17:04:05 +0300

    derp

--- generators/app/templates/view/frontend/web/requirejs-config.js
+++ generators/app/templates/view/frontend/web/requirejs-config.js
@@ -1,10 +1,12 @@
 var config = {
-    paths: {
-        '<%= context.vendor_lc %>_<%= context.module_lc %>' : '<%= context.class_name %>/js/<%= context.module_lc %>.js'
+    map : {
+        '*' : {
+            '<%= context.vendor_lc %>_<%= context.module_lc %>' : '<%= context.class_name %>/js/<%= context.module_lc %>.js'
+        }
     },
     shim : {
         '<%= context.vendor_lc %>_<%= context.module_lc %>' : {
             deps : ['jquery']
         }
     }
-}
+};
